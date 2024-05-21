f82e082487c6c379a6b1ea331ca7710df461503b
IsaacDynamo <isaacdynamo@gmail.com>
2024-05-21 22:01:01 +0200

    Test examples

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -3,3 +3,9 @@ fn compile_fail() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/*.rs");
 }
+
+#[test]
+fn examples() {
+    let t = trybuild::TestCases::new();
+    t.pass("examples/*.rs");
+}
